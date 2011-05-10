af0458bb1842983c973fb978c5cdd26b803f9d69
Chris Mair <2054847+chrismair@users.noreply.github.com>
2011-05-10 01:07:19 +0000

    Fix illegal write of final fields

--- src/main/groovy/org/gmetrics/metric/abc/AbcAstVisitor.groovy
+++ src/main/groovy/org/gmetrics/metric/abc/AbcAstVisitor.groovy
@@ -41,7 +41,7 @@ class AbcAstVisitor extends AbstractAstVisitor {
     int numberOfAssignments = 0
     int numberOfBranches = 0
     int numberOfConditions = 0
-    boolean visited = false
+    private boolean visited = false
 
     void visitMethod(MethodNode methodNode) {
         if (!isSyntheticNonRunMethod(methodNode)  && !methodNode.isAbstract()) {
