54016ebc344773430b30f98086bfbb6d0306c2a8
chrismair <chrismair@316bb773-894d-4c1b-afab-fbf10aed1fe6>
2011-10-02 20:02:02 +0000

    ClassCountMetric (partial)

---
+++ trunk/src/main/groovy/org/gmetrics/metric/classcount/ClassCountMetric.groovy
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gmetrics.metric.classcount
+
+import org.codehaus.groovy.ast.ClassNode
+import org.gmetrics.metric.AbstractMetric
+import org.gmetrics.metric.MetricLevel
+import org.gmetrics.metric.methodcount.MethodCountAstVisitor
+import org.gmetrics.result.ClassMetricResult
+import org.gmetrics.result.NumberMetricResult
+import org.gmetrics.source.SourceCode
+import org.gmetrics.util.AstUtil
+
+/**
+ * Metric for counting the number of classes within each package.
+ *
+ * @author Chris Mair
+ */
+class ClassCountMetric extends AbstractMetric {
+
+    final String name = 'ClassCount'
+    final MetricLevel baseLevel = MetricLevel.PACKAGE
+
+    protected ClassMetricResult calculateForClass(ClassNode classNode, SourceCode sourceCode) {
+        def metricResult = new NumberMetricResult(this, 1, classNode.lineNumber)
+        return new ClassMetricResult(metricResult)
+
+
+//        def visitor = new MethodCountAstVisitor(sourceCode:sourceCode)
+//        if (AstUtil.isFromGeneratedSourceCode(classNode)) {
+//            return null
+//        }
+//        visitor.visitClass(classNode)
+//        def metricResult = new NumberMetricResult(this, visitor.numberOfMethods, classNode.lineNumber)
+//        return new ClassMetricResult(metricResult)
+    }
+
+}
+
---
+++ trunk/src/test/groovy/org/gmetrics/metric/classcount/ClassCountMetricTest.groovy
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gmetrics.metric.classcount
+
+import org.gmetrics.metric.AbstractMetricTestCase
+import org.gmetrics.metric.MetricLevel
+
+/**
+ * Tests for ClassCountMetric
+ *
+ * @author Chris Mair
+ */
+class ClassCountMetricTest extends AbstractMetricTestCase {
+
+    static metricClass = ClassCountMetric
+
+    void testBaseLevelIsClass() {
+        assert metric.baseLevel == MetricLevel.PACKAGE
+    }
+
+    void testHasProperName() {
+        assert metric.name == 'ClassCount'
+    }
+
+    void testApplyToClass_SingleClass() {
+        final SOURCE = """
+            class MyClass { int myValue }
+        """
+        assertApplyToClass(SOURCE, 1)
+    }
+
+    void testApplyToClass_Enum() {
+        final SOURCE = """
+            enum MyEnum { ONE, TWO, THREE }
+        """
+        assertApplyToClass(SOURCE, 1)
+    }
+
+    void testApplyToClass_Interface() {
+        final SOURCE = """
+            interface MyInterface {
+                int doSomething(String name)
+            }
+        """
+        assertApplyToClass(SOURCE, 1)
+    }
+
+// TODO Delete - I don't think we need this
+//    void testApplyToClass_MultipleClasses() {
+//        final SOURCE = """
+//            interface MyInterface {
+//                int doSomething(String name)
+//            }
+//            class MyClass {
+//                int myValue
+//            }
+//            enum MyEnum {
+//                ONE, TWO, THREE
+//            }
+//        """
+//        assertApplyToClass(SOURCE, 3)
+//    }
+
+   void testApplyToPackage_ResultsForNoChildren() {
+        assertApplyToPackage([], 0, 0)
+    }
+
+    void testApplyToPackage_ResultsForOneChildClass() {
+        assertApplyToPackage([metricResult(1)], 1, 1)
+    }
+
+    void testApplyToPackage_ResultsForThreeChildClasses() {
+        assertApplyToPackage([metricResult(1), metricResult(1), metricResult(1)], 3, 1)
+    }
+
+    // TODO Need a way to test a mix of children: classes and sub-packages
+
+
+}
