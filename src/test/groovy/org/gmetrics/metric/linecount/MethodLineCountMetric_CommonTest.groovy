47e5d4bd41fc71bbb519a4fe4819e155bfa802ae
Chris Mair <2054847+chrismair@users.noreply.github.com>
2012-01-14 01:06:24 +0000

    Add includeClosureFields to AbstractMethodMetric

--- src/test/groovy/org/gmetrics/metric/linecount/MethodLineCountMetric_CommonTest.groovy
+++ src/test/groovy/org/gmetrics/metric/linecount/MethodLineCountMetric_CommonTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 the original author or authors.
+ * Copyright 2012 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,9 +21,8 @@ import org.gmetrics.metric.AbstractCommonMethodMetricTestCase
  * Tests for MethodLineCountMetric - common tests
  *
  * @author Chris Mair
- * @version $Revision$ - $Date$
  */
 class MethodLineCountMetric_CommonTest extends AbstractCommonMethodMetricTestCase {
-    static metricClass = MethodLineCountMetric
 
+    static metricClass = MethodLineCountMetric
 }
